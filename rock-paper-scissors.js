e1afcdef41a052b79e405acc24927560af493520
Anna <29932164+alw3979@users.noreply.github.com>
2023-08-04 16:35:27 -0500

    add a simple ui to Rock, paper, scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,7 +3,7 @@ function getComputerChoice() {
     return (choices[Math.floor(Math.random() * choices.length)])
 }
 
-function playRound(playerChoice, computerChoice) {
+function playRound(playerChoice, computerChoice=getComputerChoice()) {
     let playerChoiceLower = playerChoice.toLowerCase()
     if  (computerChoice == playerChoiceLower) {
         return "tie"
@@ -18,10 +18,68 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
+function getWinner(computerScore, playerScore) {
+    if (computerScore > playerScore){
+        return 'Computer wins, you lose!'
+    } else {
+        return 'You win, computer loses!'
+    }
+}
+
+
 let computerScore = 0
 let playerScore = 0
 
-do {
+
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+
+rockBtn.addEventListener('click', () => {
+    let outcome=playRound('rock');
+    if (outcome == 'player') {
+        playerScore+=1
+    } else if (outcome == 'computer') {
+        computerScore+=1
+    }
+    document.getElementById('scoreboard').innerHTML = 'Player score: ' + playerScore + 'Computer score:' + computerScore
+    if (computerScore == 5 || playerScore == 5) {
+        document.getElementById('scoreboard').innerHTML = getWinner(computerScore, playerScore)
+
+    }
+
+});
+
+paperBtn.addEventListener('click', () => {
+    let outcome=playRound('paper');
+    if (outcome == 'player') {
+        playerScore+=1
+    } else if (outcome == 'computer') {
+        computerScore+=1
+    }
+    if (computerScore == 5 || playerScore == 5) {
+        document.getElementById('scoreboard').innerHTML = getWinner(computerScore, playerScore)
+
+    }
+});
+
+scissorsBtn.addEventListener('click', () => {
+    let outcome=playRound('scissors');
+    if (outcome == 'player') {
+        playerScore+=1
+    } else if (outcome == 'computer') {
+        computerScore+=1
+    }
+    document.getElementById('scoreboard').innerHTML = 'Player score: ' + playerScore + 'Computer score:' + computerScore
+    if (computerScore == 5 || playerScore == 5) {
+        document.getElementById('scoreboard').innerHTML = getWinner(computerScore, playerScore)
+
+    }
+});
+
+
+
+/*do {
     let computerChoice = getComputerChoice()
     let playerChoice = prompt("rock, paper, scissors?")
     let outcome = playRound(playerChoice, computerChoice)
@@ -38,4 +96,4 @@ if (computerScore > playerScore) {
     console.log("computer wins", computerScore, playerScore)
 } else {
     console.log("playerScore", playerScore, computerScore)
-}
+} */
